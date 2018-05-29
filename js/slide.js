11c8ce59f623a1b742323255d96e02c70ddf4a99
AnnA248 <j16331an@edu.tuis.ac.jp>
2018-05-29 19:12:27 +0900

    fix

---
+++ js/slide.js
@@ -0,0 +1,35 @@
+﻿window.addEventListener('load',function(){
+
+  var imgurl=["img/slide_01.jpg", "img/slide_02.jpg", "img/slide_03.jpg"];
+  var i=0;
+  var a=1.0;
+  var timer1=null;
+  var timer2=null;
+  
+  function change (){
+    document.getElementById("slide").children[0].src=imgurl[i];
+	
+    if(i<2){
+      document.getElementById("slide").children[1].src=imgurl[i+1];
+      i++;
+	  
+    }else{
+      document.getElementById("slide").children[1].src=imgurl[0];
+      i=0;
+    }
+	
+    timer1=setInterval(function (){
+      a-=0.005;
+      document.getElementById("slide").children[0].style.opacity=a;
+    }, 1);
+	
+    if(a<0){
+      clearInterval(timer1);
+      a=1.0;
+    }
+	
+  }
+  
+  change();
+  timer2=setInterval(change, 8000);
+})
