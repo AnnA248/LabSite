f86b0875248350f7d4a98b0ed7be84f378ca7f78
AnnA248 <j16331an@edu.tuis.ac.jp>
2018-05-30 13:32:16 +0900

    fix
    fix slide

--- js/slide.js
+++ js/slide.js
@@ -1,36 +1,40 @@
-﻿
-window.addEventListener('load', setTimeout(function() {
+﻿window.addEventListener('load',setTimeout(function(){
 
-  var imgurl = ["img/slide_01.jpg", "img/slide_02.jpg", "img/slide_03.jpg"];
-  var i = 0;
-  var a = 1.0;
-  var timer1 = null;
-
-  function change1() {
+  var i=0;
+  var j=1;
+  var a=1.0;
+  var b=0.0;
+  var timer1=null;
+  
+  function change1(){
     change2();
-    setTimeout(function() {
-      requestAnimationFrame(change1)
-    }, 12000);
+    setTimeout(function(){requestAnimationFrame(change1)},14000);
   }
-
-  function change2() {
-    document.getElementById("slide").children[0].src = imgurl[i];
-    if (i < 2) {
-      document.getElementById("slide").children[1].src = imgurl[i + 1];
-      i++;
-    } else {
-      document.getElementById("slide").children[1].src = imgurl[0];
-      i = 0;
-    }
-    timer1 = setInterval(function() {
-
-      a -= 0.005;
-      document.getElementById("slide").children[0].style.opacity = a;
-    }, 1);
-    if (a < 1) {
+  function change2(){
+    timer1=setInterval(function (){
+      
+      a-=0.006;
+      b+=0.006;
+      
+      document.getElementById("slide").children[i].style.opacity=a;
+      document.getElementById("slide").children[j].style.opacity=b;
+    },1);
+    if(a<0 && b>1){
       clearInterval(timer1);
-      a = 1;
+      a=1.0;
+      b=0.0;
+
+      if(i<2){
+        i++;
+      }else{
+        i=0;
+      }
+      if(j<2){
+        j++;
+      }else{
+        j=0;
+      }
     }
   }
   change1();
-}, 8000))
+},3000))
